ef65de67cc793e1d98b4360ed18b027b63e7eb59
dagaozi <dagaozi@163.com>
2019-03-16 20:14:32 +0800

    引入基础样式，fastclick等

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from "@/pages/home/Home.vue" //@表示src目录
+import List from "@/pages/home/List.vue" //@表示src目录
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      // 当用户访问根路径时，返回helloworld组件
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/list',
+      name: 'List',
+      component: List
     }
   ]
 })
