9d66c607c5d53b607b17a2e5486a83cbc6d32777
dagaozi <dagaozi@163.com>
2019-04-09 20:43:39 +0800

    swiper

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="header">
     <div class="header-left"><div class="iconfont  back-icon">&#xe624;</div></div>
-    <div class="header-input"><spn class="iconfont">&#xe632;</spn>收入城市/景点/游玩主题</div>
-    <div class="header-right">城市<spn class="iconfont arrow-icon">&#xe64a;</spn></div>
+    <div class="header-input"><span class="iconfont">&#xe632;</span>收入城市/景点/游玩主题</div>
+    <div class="header-right">城市<span class="iconfont arrow-icon">&#xe64a;</span></div>
     </div>
 </template>
 <script>
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,16 +1,10 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption" ref="mySwiper" @someSwiperEvent="callback">
-      <swiper-slide>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
         <img
           class="swiper-img"
-          src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg"
-        >
-      </swiper-slide>
-      <swiper-slide>
-        <img
-          class="swiper-img"
-          src="http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/9f73976e40c4ef845cabe0efc0269ebb.jpg_750x200_aab92b7a.jpg"
+          :src="item.imgUrl"
         >
       </swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
@@ -21,21 +15,35 @@
 <script>
 export default {
   name: "HomeSwiper",
-  data() {
+  data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination:'.swiper-pagination',
+        loop:true
+      },
+      swiperList:[{
+        id:"0001",
+        imgUrl:"http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/d7bbc21db442366a882e04ddc984669a.jpg_750x200_85e640d9.jpg"
+      },
+      {
+        id:"0002",
+        imgUrl:"http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/9f73976e40c4ef845cabe0efc0269ebb.jpg_750x200_aab92b7a.jpg"
+      }]
     };
   }
 };
 </script>
 
 <style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active //样式穿透，wrapper下面的.swiper-pagination-bullet-active 不受scoped限制
+  background #FFF 
 .wrapper
   overflow hidden
   width 100%
   height 0
-  padding-bottom 31.25%
+  padding-bottom 27.25%  //上面四个可以保证图片宽高比为27.25%
+  background #eee
   .swiper-img
-  width 100%
+   width 100%
 </style>
 
