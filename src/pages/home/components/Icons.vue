dc4f42388f08cc9c3f0090fac362f344f1a87694
dagaozi <dagaozi@163.com>
2019-04-10 11:20:46 +0800

    icon菜单页多页，通过计算属性计算icon所属页数

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="icons">
     <swiper>
-      <swiper-slide>
-        <div class="icon" v-for="item of iconList" :key="item.id">
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
           <div class="icon-img">
             <img class="icon-imgcontent" :src='item.imgUrl'>
           </div>
@@ -20,7 +20,7 @@ export default {
           iconList:[
               {id:'0001',
               imgUrl:'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
-              desc:'景点门票'},
+              desc:'景点门票景点门票景点门票'},
               {
               id:'0002',
               imgUrl:'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/a40ee278d67000f2a29d2e20f6a029b3.png',
@@ -61,11 +61,26 @@ export default {
           ]
       }
   },
+  computed:{
+      /**把一维数组变成二维数组（每个小维度中八个icon） */
+      pages(){
+          const pages=[]
+          this.iconList.forEach((itme,index)=>{
+              const page=Math.floor(index/8) //8个icon为一页，计算当前icon属于第几页
+              if(!pages[page]){ //如果pages第0页为空，则为pages[0]赋值一个空数组
+                  pages[page]=[]
+              }
+              pages[page].push(itme)//往二维数组里面填值
+          })
+          return pages
+      }
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/base.styl'
+@import '~styles/fontMixins.styl'
 .icons >>>.swiper-container  //不写swiper-container的高度，无法占满整个vue组件高度，只占一个icon的高度，导致如果只有一行，下半部分无法滑动
   width 100% // div可以不用写，默认充满
   overflow hidden  //可以不写，swiper控件自带了overflow:hidden
@@ -85,7 +100,7 @@ export default {
       top 0
       left 0
       right 0
-      bottom 0.44rem
+      bottom 0.22rem
       box-sizing border-box
       padding 0.1rem
       //background blue
@@ -102,4 +117,5 @@ export default {
     line-height 0.44rem
     text-align center
     color $darkTextColor
+    elipsis() //引用定义的基础样式
 </style>
