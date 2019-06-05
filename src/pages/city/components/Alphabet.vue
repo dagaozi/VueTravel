e169e4fdedb58f0e947453f7e074192307f2e7d8
dagaozi <dagaozi@163.com>
2019-06-05 10:49:02 +0800

    city-ajax请求，v-for双层循环

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
     <ul class="list">
-      <li class="item">A</li>
-      <li class="item">B</li>
-      <li class="item">B</li>
-      <li class="item">B</li>
-      <li class="item">B</li>
-      <li class="item">B</li>
-      <li class="item">B</li>
+      <li class="item" v-for="(item, key) in cities" :key="key">{{key}}</li>
     </ul>
   </div>
 </template>
 <script>
 export default {
-  name:'Alphabet'
+  name:'Alphabet',
+  props:{
+    cities:Object
+  }
 }
 </script>
 <style lang="stylus" scoped>
