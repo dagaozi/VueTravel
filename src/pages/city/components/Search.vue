6e25f47c1d404bde247861bbc3eb7cd9ddcc38d8
dagaozi <dagaozi@163.com>
2019-06-09 13:48:47 +0800

    search-city

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,20 +1,87 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+    <!-- keyword有值显示搜索内容 -->
+    <div class="search-content" ref='search' v-show="this.keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item in list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="this.hasNoData">没有该城市</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name:"CitySearch"
-}
+  name: "CitySearch",
+  props:{
+    cities:Object
+  },
+  data() {
+    return {
+      keyword:"",
+      list:[],
+      timer:null,
+    }
+  },
+  computed: {
+    hasNoData(){
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword(){
+      if(this.timer){
+        clearTimeout(this.timer)
+      }
+      if(!this.keyword){
+        this.list=[]
+        return
+      }
+      this.timer=setTimeout(() => {
+        const result=[]
+        console.log(this.cities);
+        console.log(this.keyword);
+        
+        for (const i in this.cities) {
+          this.cities[i].forEach(element => {
+            if(element.spell.indexOf(this.keyword)>-1 || element.name.indexOf(this.keyword)>-1){
+              result.push(element)
+            }
+          })
+        }
+        this.list=result
+      }, 100);
+    }
+  },
+  mounted() {
+    this.scroll=new Bscroll(this.$refs.search)
+  },
+};
 </script>
 <style lang="stylus" scoped>
-  @import '~styles/base.styl'
-  .search
-    height .62rem
-    padding .1rem
-    background $baseColor
-    .search-input
-      inputStyl()
+@import '~styles/base.styl'
+.search
+  height 0.62rem
+  padding 0.1rem
+  background $baseColor
+  .search-input
+    inputStyl()
+.search-content
+  z-index 1
+  overflow hidden
+  position absolute
+  top 1.68rem
+  left 0
+  right 0
+  bottom 0
+  background #eee
+  .search-item
+    line-height .62rem
+    padding-left .2rem
+    color $fontColor
+    background #fff
 </style>
 
